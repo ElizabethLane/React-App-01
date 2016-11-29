5766056cbb5df4ea9eef8eaca4be98a1d2af612c
Elizabeth Nelson <elizabeth.larkin.nelson@gmail.com>
2016-11-28 16:20:01 -0800

    Addition of prop arg in greeter function and DOM react render

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,21 @@
+var Greeter = React.createClass({
+  render: function () {
+    var name = this.props.name;
+
+    return (
+      <div>
+        <h1> Hello React!</h1>
+        <p> This is from the component </p>
+      </div>
+
+    );
+  }
+});
+
+
 ReactDOM.render(
-  <h1> Hello React!</h1>,
+  <Greeter name="Liz"/>,
+  //The "name" attribute is a prop
+  //See line 3
   document.getElementById('app')
 );
