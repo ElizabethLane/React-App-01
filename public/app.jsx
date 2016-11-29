7d39adc26f859f0e37cd6f8a60f5d494959f42b7
Elizabeth Nelson <elizabeth.larkin.nelson@gmail.com>
2016-11-28 16:57:38 -0800

    Button, preventdefault, alert name all work

--- public/app.jsx
+++ public/app.jsx
@@ -1,21 +1,44 @@
 var Greeter = React.createClass({
+  getDefaultProps: function(){
+    return {
+      name: 'React',
+      message: 'This is the default message'
+    }
+  },
+  onButtonClick: function(e) {
+    e.preventDefault();
+
+    var name = this.refs.name.value;
+
+    alert(name);
+  },
   render: function () {
     var name = this.props.name;
+    var message = this.props.message;
 
     return (
       <div>
-        <h1> Hello React!</h1>
-        <p> This is from the component </p>
+        <h1> Hello {name}!</h1>
+        <p> {message}</p>
+        <form onSubmit={this.onButtonClick}>
+          <input type='text' ref='name'/>
+          <button>Set Name</button>
+        </form>
       </div>
 
     );
   }
 });
 
+var firstName = 'Liz';
+var message = "Welcome to the coolest message ever"
 
 ReactDOM.render(
-  <Greeter name="Liz"/>,
+  <Greeter name={firstName} message={message}/>,
   //The "name" attribute is a prop
   //See line 3
+  //Here we are passing a name in
+  //getDefaultProps allows this attribute(prop)
+  //to have a default value if none specified
   document.getElementById('app')
 );
