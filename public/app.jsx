9c640afa909d46074611ba9546ce6422611f0808
Elizabeth Nelson <elizabeth.larkin.nelson@gmail.com>
2016-12-01 14:35:39 -0800

    added responsive text area

--- public/app.jsx
+++ public/app.jsx
@@ -17,17 +17,27 @@ var GreeterForm = React.createClass({
   onFormSubmit: function(e){
     e.preventDefault;
 
+    var updates = {};
     var name = this.refs.name.value;
+    var message = this.refs.message.value;
 
     if (name.length > 0) {
       this.refs.name.value = '';
-      this.props.onNewName(name);
+      updates.name = name;
     }
+
+    if (message.length > 0){
+      this.refs.message.value = '';
+      updates.message = message;
+    }
+
+    this.props.onNewName(updates);
   },
   render: function(){
     return (
       <form onSubmit={this.onFormSubmit}>
-        <input type='text' ref='name'/>
+        <input type='text' ref='name' placehold="Enter name"/>
+        <textarea ref='message' placeholder="Enter message"></textarea>
         <button>Set Name</button>
       </form>
     );
@@ -42,18 +52,17 @@ var Greeter = React.createClass({
   },
   getInitialState: function(){
       return {
-        name: this.props.name
+        name: this.props.name,
+        message: this.props.message
       }
     },
 
-  handleNewName: function(name) {
-    this.setState({
-      name: name
-    });
+  handleNewName: function(updates) {
+    this.setState(updates});
   },
   render: function () {
     var name = this.state.name;
-    var message = this.props.message;
+    var message = this.state.message;
 
     return (
       <div>
