7a238d7ab218c1f43935507fa0f6a93c1c163f9a
Elizabeth Nelson <elizabeth.larkin.nelson@gmail.com>
2016-12-12 14:54:37 -0800

    All greeter components complete

--- public/app.jsx
+++ public/app.jsx
@@ -1,39 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var GreeterMessage = require('./components/GreeterMessage');
-//Can leave off .jsx on module name, because in webpack config
-//specified extensions we want to support
-var GreeterForm = require('./components/GreeterForm');
-
-var Greeter = React.createClass({
-  getDefaultProps: function(){
-    return {
-      name: 'React',
-      message: 'This is the default message'
-    }
-  },
-  getInitialState: function(){
-      return {
-        name: this.props.name,
-        message: this.props.message
-      }
-    },
-
-  handleNewData: function(updates) {
-    this.setState(updates);
-  },
-  render: function () {
-    var name = this.state.name;
-    var message = this.state.message;
-
-    return (
-      <div>
-        <GreeterMessage name={name} message={message}/>
-        <GreeterForm onNewData={this.handleNewData}/>
-      </div>
-    );
-  }
-});
+var Greeter = require('./components/Greeter');
 
 var firstName = 'Liz';
 var message = "Welcome to the coolest message ever"
--- public/bundle.js
+++ public/bundle.js
@@ -48,42 +48,7 @@
 
 	var React = __webpack_require__(1);
 	var ReactDOM = __webpack_require__(158);
-	var GreeterMessage = __webpack_require__(159);
-	//Can leave off .jsx on module name, because in webpack config
-	//specified extensions we want to support
-	var GreeterForm = __webpack_require__(160);
-
-	var Greeter = React.createClass({
-	  displayName: 'Greeter',
-
-	  getDefaultProps: function getDefaultProps() {
-	    return {
-	      name: 'React',
-	      message: 'This is the default message'
-	    };
-	  },
-	  getInitialState: function getInitialState() {
-	    return {
-	      name: this.props.name,
-	      message: this.props.message
-	    };
-	  },
-
-	  handleNewData: function handleNewData(updates) {
-	    this.setState(updates);
-	  },
-	  render: function render() {
-	    var name = this.state.name;
-	    var message = this.state.message;
-
-	    return React.createElement(
-	      'div',
-	      null,
-	      React.createElement(GreeterMessage, { name: name, message: message }),
-	      React.createElement(GreeterForm, { onNewData: this.handleNewData })
-	    );
-	  }
-	});
+	var Greeter = __webpack_require__(159);
 
 	var firstName = 'Liz';
 	var message = "Welcome to the coolest message ever";
@@ -19793,6 +19758,52 @@
 	'use strict';
 
 	var React = __webpack_require__(1);
+	var GreeterMessage = __webpack_require__(160);
+	//Can leave off .jsx on module name, because in webpack config
+	//specified extensions we want to support
+	var GreeterForm = __webpack_require__(161);
+
+	var Greeter = React.createClass({
+	  displayName: 'Greeter',
+
+	  getDefaultProps: function getDefaultProps() {
+	    return {
+	      name: 'React',
+	      message: 'This is the default message'
+	    };
+	  },
+	  getInitialState: function getInitialState() {
+	    return {
+	      name: this.props.name,
+	      message: this.props.message
+	    };
+	  },
+
+	  handleNewData: function handleNewData(updates) {
+	    this.setState(updates);
+	  },
+	  render: function render() {
+	    var name = this.state.name;
+	    var message = this.state.message;
+
+	    return React.createElement(
+	      'div',
+	      null,
+	      React.createElement(GreeterMessage, { name: name, message: message }),
+	      React.createElement(GreeterForm, { onNewData: this.handleNewData })
+	    );
+	  }
+	});
+
+	module.exports = Greeter;
+
+/***/ },
+/* 160 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var React = __webpack_require__(1);
 
 	var GreeterMessage = React.createClass({
 	  displayName: 'GreeterMessage',
@@ -19831,7 +19842,7 @@
 	//you get back
 
 /***/ },
-/* 160 */
+/* 161 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
